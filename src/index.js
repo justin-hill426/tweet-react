70fd9fb55eb4b0ee8968dc41aa98e85f1e4f8bd4
justin-hill426 <jrhill426@gmail.com>
2022-01-03 16:09:23 -0700

    Added Tweet Component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+const Time = () => <span className='time'>3hr ago</span>
+const ReplyButton = () => <i className='fa fa-reply reply-button'/>
+const RetweetButton = () => <i className='fa fa-retweet retweet-button'/>
+const LikeButton = () => <i className='fa fa-heart like-button'/>
+const MoreOptionsButton = () => (
+  <i className='fa fa-ellipsis-h more-options-button'/>
+)
+function Tweet() {
+  return (
+    <div className="tweet">
+      <Avatar/>
+      <div className='content'>
+        <Author />
+        <Message />
+        <div className='buttons'>
+          <ReplyButton/>
+          <RetweetButton/>
+          <LikeButton/>
+          <MoreOptionsButton/>
+        </div>
+      </div>
+    </div>
+  )
+}
+ReactDOM.render(<Tweet />, document.querySelector('#root'))
+function Avatar() {
+  return (
+    <img
+    src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+    className="avatar"
+    alt="avatar"
+    >
+    </img>
+  )
+}
+function Message() {
+  return <div className='message'>This is less than 140 characters</div>
+}
+function Author() {
+  return (
+    <span className='author'>
+      <span className='name'>Your Name</span>
+      <span className='handle'>@yourhandle</span>
+    </span>
+  )
+}
