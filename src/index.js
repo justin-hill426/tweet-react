61adf7a0128d85a63d5c600d7be42937e139ff34
justin-hill426 <jrhill426@gmail.com>
2022-01-04 11:38:45 -0700

    Reworked props and added moment

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import moment from 'moment'
 import './index.css'
-const Time = () => <span className='time'>3hr ago</span>
+const Time = ({time}) => {
+  const timeString = moment(time).fromNow()
+  return <span className="time">{timeString}</span>
+}
 const ReplyButton = () => <i className='fa fa-reply reply-button'/>
 const RetweetButton = () => <i className='fa fa-retweet retweet-button'/>
 const LikeButton = () => <i className='fa fa-heart like-button'/>
 const MoreOptionsButton = () => (
   <i className='fa fa-ellipsis-h more-options-button'/>
 )
-function Tweet() {
+function Tweet({tweet}) {
   return (
     <div className="tweet">
-      <Avatar/>
+      <Avatar hash={tweet.gravatar} />
       <div className='content'>
-        <Author />
-        <Message />
+        <Author author = {tweet.author}/>
+        <Time />
+        <Message message={tweet.message} />
         <div className='buttons'>
           <ReplyButton/>
           <RetweetButton/>
@@ -25,25 +30,37 @@ function Tweet() {
     </div>
   )
 }
-ReactDOM.render(<Tweet />, document.querySelector('#root'))
-function Avatar() {
+const testTweet = {
+  message: 'Something about cats.',
+  gravatar: 'xyz',
+  author: {
+    handle: 'catperson',
+    name: 'IAMA Cat Person',
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: '2016-07-30 21:24:37',
+}
+ReactDOM.render(<Tweet tweet = {testTweet} />, document.querySelector('#root'))
+function Avatar({hash}) {
+  const url = `https://www.gravatar.com/avatar/${hash}`
   return (
     <img
-    src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+    src={url}
     className="avatar"
     alt="avatar"
     >
     </img>
   )
 }
-function Message() {
-  return <div className='message'>This is less than 140 characters</div>
+function Message({message}) {
+  return <div className='message'>{message}</div>
 }
-function Author() {
+function Author({author}) {
   return (
     <span className='author'>
-      <span className='name'>Your Name</span>
-      <span className='handle'>@yourhandle</span>
+      <span className='name'>{author.name}</span>
+      <span className='handle'>@{author.handle}</span>
     </span>
   )
 }
